b294fff246ad5f12e7bf0e22796d09f714b1bfe9
Nikolay Stoynov <arvigeus@gmail.com>
2021-11-20 23:20:17 +0200

    Fix graphql

--- src/routes/Graphql.tsx
+++ src/routes/Graphql.tsx
@@ -1,5 +1,4 @@
 import { languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-markdown";
 import "prismjs/components/prism-graphql";
 import parserGraphql from "prettier/parser-graphql";
 
