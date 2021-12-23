2b56c1bcf1b15658fc5d76a7d2c5546e66e0cf0d
Nikolay Stoynov <arvigeus@gmail.com>
2021-12-23 15:32:19 +0200

    Make editor uncontrolled

--- app/routes/lang.tsx
+++ app/routes/lang.tsx
@@ -18,16 +18,28 @@ export let links: LinksFunction = () => {
 export type FormatterFunction = (text: string) => string;
 
 export default function Format() {
-  const [code, handleChange] = useState("");
+  const [code, setCode] = useState("");
+  const value = useRef<string>("");
   const format = useRef<FormatterFunction>();
   const toggleTheme = useFetcher<Record<string, string>>();
 
   const setFormatter = useCallback((func: FormatterFunction) => {
     format.current = func;
   }, []);
+  const handleChange = useCallback((input: string) => {
+    value.current = input;
+  }, []);
   const handleFormat = useCallback(() => {
-    if (!format.current) return;
-    handleChange(format.current(code));
+    if (!format.current || !value.current || value.current === code) return;
+    value.current = format.current(value.current);
+    if (value.current !== code) setCode(value.current);
+    else {
+      // Edge case: Only formatting was changed (this would not trigger re-render)
+      // Use a dummy value to force update code
+      setCode(value.current + " ");
+      // Then use delay to immidiately correct it
+      setTimeout(() => setCode(value.current.slice(0, -1)), 0);
+    }
   }, [code]);
   const copyToClipboard = useCallback(() => {
     navigator.clipboard.writeText(code);
@@ -35,7 +47,7 @@ export default function Format() {
 
   return (
     <div style={toggleTheme.data}>
-      <Outlet context={{ handleChange, setFormatter, code }} />
+      <Outlet context={{ handleChange: handleChange, setFormatter, code }} />
       <toggleTheme.Form method="post" action="/toggleTheme">
         <Panel
           onFormat={handleFormat}
